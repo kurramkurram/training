9e054679a382e68a8f0759487a6a8b61eb688d53
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-04-07 00:59:03 +0900

    [Update] use lifecycleScope

--- networkconnection/app/src/main/java/com/example/networkconnection/MainActivity.kt
+++ networkconnection/app/src/main/java/com/example/networkconnection/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.networkconnection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.example.networkconnection.retrofit.BASE_URL
 import com.example.networkconnection.retrofit.PersonService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,23 +19,26 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "MainActivity"
     }
 
-    private val retrofit = Retrofit.Builder().baseUrl(BASE_URL).build()
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
 
     private val service = retrofit.create(PersonService::class.java)
-
-    private val loadPerson = service.loadPerson("Shakespeare")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val response = loadPerson.clone().execute()
-
-            CoroutineScope(Dispatchers.Main).launch {
-                if (response.isSuccessful) {
-                    findViewById<TextView>(R.id.textview).text = response.body()?.string() ?: ""
+        lifecycleScope.launch {
+            try {
+                val response = service.loadPerson("Shakespeare")
+                Log.d(TAG, "#onCreate $response")
+                CoroutineScope(Dispatchers.Main).launch {
+                    findViewById<TextView>(R.id.textview).text = response.data.name
                 }
+            } catch (e: Exception) {
+                Log.e(TAG, "#onCreate\$lifecycleScope $e")
             }
         }
     }
